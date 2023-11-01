a2361c3cabd1b6d9c3063fb5121b745e25581f4f
SystemAdmin12345 <102834957+SystemAdmin12345@users.noreply.github.com>
2023-11-01 15:38:50 -0400

    Update OwnModule.lua

--- SS/Main Scripts and hubs/OwnModule.lua
+++ SS/Main Scripts and hubs/OwnModule.lua
@@ -4,7 +4,7 @@ require(15210774178):ship() -- Loads Ship
 require(15210774178):zero() -- Crashes Server
 require(15210774178):shutdown() -- Shutdown
 require(15210774178):owner() -- Owner Only
-require(15210774178):gamer() -- Another Hub, owner only
-require(15210774178):oldgamer() -- classic version
+require(15210774178).gamer("Username") -- A script hub, works for anyone (do not use owner only scripts)
+require(15210774178):oldgamer() -- classic version, owner only
 require(15210774178):sus() -- Group only
 -- Note: sometimes works, sometimes doesn't
